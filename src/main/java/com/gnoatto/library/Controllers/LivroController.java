1019de96f4b78d97b6aa68300e8d8c1bb6c13cce
Gabriel Gnoatto <gnoattogabriel100@gmail.com>
2026-03-05 16:58:58 -0300

    Livro Controller criado

---
+++ src/main/java/com/gnoatto/library/Controllers/LivroController.java
@@ -0,0 +1,43 @@
+package com.gnoatto.library.Controllers;
+
+
+import com.gnoatto.library.Models.LivroModel;
+import com.gnoatto.library.Services.LivroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/livros")
+public class LivroController {
+
+    @Autowired
+    private LivroService livroService;
+
+    @PostMapping
+    public LivroModel criarLivro(@RequestBody LivroModel livroNovo){
+        return livroService.criarLivro(livroNovo);
+    }
+
+    @GetMapping
+    public List<LivroModel> buscarTodosLivros(){
+        return livroService.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarLivro(@PathVariable Long id){
+        livroService.deletarLivro(id);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<LivroModel> buscarPorId(@PathVariable Long id){
+        return livroService.buscarPorId(id);
+    }
+
+    public LivroModel atualizarLivro(@PathVariable Long id,@RequestBody LivroModel livroNovo){
+        return livroService.atualizarLivro(id, livroNovo);
+    }
+}
